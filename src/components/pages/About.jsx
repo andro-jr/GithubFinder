80988535ff67565316a57947cb7f9c6af84e7844
prabin <prabin20panta@gmail.com>
2023-05-09 12:58:15 +0545

    layout set

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className='text-6xl mb-4'>GithubFinder</h1>
+      <p className='mb-4 text-2xl font-light'>Hello Darkness my old Friend.</p>
+    </div>
+  );
+};
+
+export default About;
